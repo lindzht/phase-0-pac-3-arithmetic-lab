d4e1920846b53ab1f002b036a454f1a94727f770
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-07-31 13:01:02 -0400

    commit tree d5d59ee565f659511548e5eae9cea586b65283ca

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(num1, num2){
+    return num1 + num2;
+};
+
+function subtract(a, b){
+    return a - b;
+};
+
+function multiply (a, b){
+    return  a*b;
+};
+
+function divide(a, b){
+    return a/b;
+};
+
+function increment(number){
+    return (number += 1);
+};
+
+function decrement(number){
+    return (number -= 1);
+}
+
+function makeInt(number){
+    return parseInt(number, 10);
+}
+
+function preserveDecimal(number){
+    return parseFloat(number);
+}
